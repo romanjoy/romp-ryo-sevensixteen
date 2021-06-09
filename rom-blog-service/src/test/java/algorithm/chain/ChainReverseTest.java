b11ec0e87c952b4731327df277e330cdba405a81
fuqiliang <fuqiliang@wedochina.cn>
2021-06-09 14:25:53 +0800

    Chain Reverse updated  1. add recursion method  2. delete old ChainBase.java

--- rom-blog-service/src/test/java/algorithm/chain/ChainReverseTest.java
+++ rom-blog-service/src/test/java/algorithm/chain/ChainReverseTest.java
@@ -14,8 +14,20 @@ public class ChainReverseTest {
         ListNode<String> lai = new ListNode<>(zi, "来");
         ListNode<String> shui = new ListNode<>(lai, "水");
 
-        String reverse = ChainReverse.reverse(shui);
-        System.out.println(reverse);
-        Assert.assertNotNull(reverse);
+        System.out.println(ChainReverse.reverse(shui).toString());
+        Assert.assertNotNull(ChainReverse.reverse(shui).toString());
+    }
+
+    @Test
+    public void chainReverseByRec() {
+        /* 水来自海上 -> 上海自来水 */
+        ListNode<String> shang = new ListNode<>(null, "上");
+        ListNode<String> hai = new ListNode<>(shang, "海");
+        ListNode<String> zi = new ListNode<>(hai, "自");
+        ListNode<String> lai = new ListNode<>(zi, "来");
+        ListNode<String> shui = new ListNode<>(lai, "水");
+
+        System.out.println(ChainReverse.reverseByRec(shui).toString());
+        Assert.assertNotNull(ChainReverse.reverseByRec(shui).toString());
     }
 }
