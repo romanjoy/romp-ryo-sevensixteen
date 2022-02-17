92a2b8e02f124a24653aa7feb565d390295068e7
romanjoy <fuqiliang_8@163.com>
2022-02-17 16:56:46 +0800

    Java Concurrency   - Worker threads run in CyclicBarrier

---
+++ rom-blog-service/src/main/java/concurrency/CyclicBarrierForWorker.java
@@ -0,0 +1,44 @@
+package concurrency;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Worker threads run in CyclicBarrier <br>
+ *
+ * @author Roman Fu
+ * @version 1.0
+ */
+public class CyclicBarrierForWorker {
+
+    private static class Worker extends Thread {
+        private final CyclicBarrier cyclicBarrier;
+
+        public Worker(CyclicBarrier cyclicBarrier) {
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        public void run() {
+            super.run();
+            try {
+                System.out.println(Thread.currentThread().getName() + "start to wait other thread...");
+                cyclicBarrier.await();
+                System.out.println(Thread.currentThread().getName() + " start to execute...");
+                Thread.sleep(3000);  // do something
+                System.out.println(Thread.currentThread().getName() + "execute over.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+         int threadCount = 3;
+         CyclicBarrier cyclicBarrier = new CyclicBarrier(threadCount);
+
+         for (int i=0; i<threadCount; i++) {
+             System.out.println("Worker thread will be created,No." + i);
+             Worker worker = new Worker(cyclicBarrier);
+             worker.start();
+         }
+    }
+}
