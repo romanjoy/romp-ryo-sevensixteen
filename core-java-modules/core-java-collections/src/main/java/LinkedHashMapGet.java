cb090f136f0d3d9c9383c76c54f0cb4f2d025464
romanjoy <fuqiliang_8@163.com>
2022-05-11 11:46:56 +0800

    Access/Insert- order element get in LinkedHashMap

--- core-java-modules/core-java-collections/src/main/java/LinkedHashMapGet.java
+++ core-java-modules/core-java-collections/src/main/java/LinkedHashMapGet.java
@@ -1,15 +1,13 @@
-package com.example.rom.collections;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * access/insert first in LinkedHashMap
+ * Access/insert first in LinkedHashMap
  *
  * @author Roman Fu
  * @version 1.0
  */
-public class LinkedHashMapFirst {
+public class LinkedHashMapGet {
 
     public static void main(String[] args) {
         LinkedHashMap<Integer, String> lHashMap = new LinkedHashMap<>(5);  // insert first
@@ -20,10 +18,10 @@ public class LinkedHashMapFirst {
         lHashMap.put(5, "堀北真希");
 
         for (Map.Entry<Integer, String> next : lHashMap.entrySet()) {
-            System.out.println("index=" + next.getKey() + ",value=" + next.getValue());
+            System.out.println("index=" + next.getKey() + ", value=" + next.getValue());
         }
 
-        System.out.println("\n");
+        System.out.print("\n");
 
         LinkedHashMap<Integer, String> lHashMap2 = new LinkedHashMap<>(5, 0.75f, true);
         lHashMap2.put(1, "新恒结衣");
@@ -31,12 +29,11 @@ public class LinkedHashMapFirst {
         lHashMap2.put(3, "佐佐木希");
         lHashMap2.put(4, "石原里美");
         lHashMap2.put(5, "堀北真希");
-        lHashMap2.get(3);
+        lHashMap2.get(3);  // access-order
         lHashMap2.get(1);
 
-
         for (Map.Entry<Integer, String> next : lHashMap2.entrySet()) {
-            System.out.println("index=" + next.getKey() + ",value=" + next.getValue());
+            System.out.println("index=" + next.getKey() + ", value=" + next.getValue());
         }
     }
 }
